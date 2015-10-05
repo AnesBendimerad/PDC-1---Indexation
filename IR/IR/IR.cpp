c020f76bab9b6108806a3044924b5772b6f32ec3
Hassan El-Moutaraji <hassan.elmoutaraji@gmail.com>
2015-10-05 22:32:35 +0200

    Hello world from VS !

---
+++ IR/IR/IR.cpp
@@ -0,0 +1,16 @@
+// IR.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World !" << endl;
+	getchar();
+
+	return 0;
+}
+
