511d5dde8d4759be9041969dfa9881053d468d0b
BelfodilAimene <ba_belfodil@esi.dz>
2015-10-06 23:39:22 +0200

    [Aymen] add Hasher.cpp

---
+++ IR/IR/Hasher.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Hasher.h"
+
+int Hasher::hash(string token)
+{
+	int resultat = 0;
+	for (int i = 0; i < token.length(); i++)
+	{
+		resultat <<= 2;
+		resultat += token[i];
+	}
+	return resultat;
+}
