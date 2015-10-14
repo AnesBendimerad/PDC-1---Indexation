7eaf74c112eaa35ee486b77bd602b805d3e116d2
Hassan El-Moutaraji <hassan.elmoutaraji@gmail.com>
2015-10-14 21:58:05 +0200

    [Hassan] second version of tokenizer
    [Hassan] second version of tokenizer

---
+++ IR/IR/stopWords.h
@@ -0,0 +1,42 @@
+#include<unordered_set>
+
+std::unordered_set<std::string> * getStopWords(){
+
+	std::unordered_set<std::string> * stopWords = new std::unordered_set<std::string>();
+
+	stopWords->insert("<p>");
+	stopWords->insert("</p>");
+	stopWords->insert("and");
+	stopWords->insert("about");
+	stopWords->insert("an");
+	stopWords->insert("are");
+	stopWords->insert("as");
+	stopWords->insert("at");
+	stopWords->insert("be");
+	stopWords->insert("by");
+	stopWords->insert("com");
+	stopWords->insert("for");
+	stopWords->insert("from");
+	stopWords->insert("how");
+	stopWords->insert("in");
+	stopWords->insert("is");
+	stopWords->insert("it");
+	stopWords->insert("of");
+	stopWords->insert("on");
+	stopWords->insert("or");
+	stopWords->insert("that");
+	stopWords->insert("the");
+	stopWords->insert("this");
+	stopWords->insert("to");
+	stopWords->insert("was");
+	stopWords->insert("what");
+	stopWords->insert("when");
+	stopWords->insert("where");
+	stopWords->insert("who");
+	stopWords->insert("will");
+	stopWords->insert("with");
+	stopWords->insert("the");
+	stopWords->insert("www");
+
+	return stopWords;
+}
