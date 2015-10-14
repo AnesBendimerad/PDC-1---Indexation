78a49be7e0057c0ec615232596529b7f835b37e6
AnesBendimerad <bendimeradanes@gmail.com>
2015-10-14 22:31:15 +0200

    [Anes] fi khater hassan 2

--- IR/IR/IR.cpp
+++ IR/IR/IR.cpp
@@ -19,8 +19,13 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	
-	Indexer * indexer = new Indexer("C:\\myFiles\\tests");
-	IIndex  * index = indexer->createIndex();
+	//Indexer * indexer = new Indexer("C:\\myFiles\\tests");
+	//IIndex  * index = indexer->createIndex();
+	Term t;
+	t.documentNumber = 1;
+	t.token = "";
+	t.totalFrequency = 1;
+	int size = sizeof(t.token);
 	cout << "the end" << endl;
 	
 	return 0;
