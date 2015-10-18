eb6965aa32fd763fef2b64869f81eedf3cbd08fe
Maha Benabbou <maha.benabbou93@gmail.com>
2015-10-18 22:49:32 +0200

    [Maha]: Add implementation of fagin's algorithm

--- IR/IR/Index.cpp
+++ IR/IR/Index.cpp
@@ -1,13 +1,15 @@
 #include "stdafx.h"
 #include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cmath> 
+#include <algorithm>
 #include "Index.h"
 #include "IDictionary.h"
 #include "DocumentTable.h"
 #include "DocumentTerm.h"
 #include "Term.h"
-#include <iostream>
-
-
+using namespace std;
 
 Index::Index(IDictionary *dictionary, string outputFilePath)
 {
@@ -54,7 +56,7 @@ void Index::addTerm(string token)
 void Index::finalize()
 {
 	if (!finalized) {
-		documentTable->finalize();
+	    documentTable->finalize();
 		// write the posting list on file and change the pointer to offset in file (postingFilePath)
 		// the offstream structure file is :
 		//		Dictionary offset
@@ -65,11 +67,11 @@ void Index::finalize()
 		//		Dictionary
 		ofstream outputFile(postingFilePath);
 		// prepare a place for the dictionary offset
-		outputFile.write((const char *)nullptr, sizeof(unsigned int));
+		//outputFile.write((const char *)nullptr, sizeof(void *));
 		// write the terms number in Dictionary
-		outputFile.write((const char *)&dictionary->getTermsNumber(), sizeof(unsigned long long));
+		outputFile.write((const char *) &dictionary->getTermsNumber(), sizeof(unsigned long long));
 		// write the DocumentMetaDatas number in DocumentTable
-		outputFile.write((const char *)&documentTable->getDocumentNumber(), sizeof(unsigned long long));
+		outputFile.write((const char *) &documentTable->getDocumentNumber(), sizeof(unsigned int));
 		// write the DocumentMetaDatas
 		outputFile.write((const char *)documentTable->getFinalizedDocumentTable(),documentTable->getDocumentNumber()*sizeof(DocumentMetaData));
 		// write the posting lists contiguously
@@ -90,30 +92,16 @@ void Index::finalize()
 			free(docTermTable);
 		}
 		delete termIterator;
-		// save the dictionary offset
-		unsigned int * dictionaryOffset= (unsigned int *)(unsigned int)outputFile.tellp();
 		// write the terms of the dictionary
-		termIterator = dictionary->getIterator();
-		while ((term = static_cast<Term*>(termIterator->getNext())) != nullptr) {
-			outputFile.write((const char *)&term, sizeof(Term));
-		}
-		outputFile.seekp(0);
-		outputFile.write((const char *)dictionaryOffset, sizeof(unsigned int));
+		// TODO
 		outputFile.close();
 		finalized = true;
-		
 	}
 	
 	else 
 	{
 		throw runtime_error("Index finalized yet");
 	}
-}
-
-list<int> Index::search(string querry)
-{
-	// return the most relevent  document to the query
-	return list<int>();
 }
 
 DocumentTerm * Index::getTermPostingList(string token)
@@ -139,4 +127,140 @@ Index::~Index()
 	delete documentTable;
 }
 
+// This function implements fagin's algorithm and return the topK documents which matchs the query
+list<int> Index::search(int topK, string query)
+{
+	vector<vector<pair<int, double>>> list_sorted_by_tf_idf = calculateTF_IDF(query);
+	vector<pair<int, double>> topKDocuments;
+	list<int> topDocuments;
 
+	double thresHold;
+	double weight=0, globalWeight =0;
+	int documentIndex;
+	int result;
+
+	do
+	{
+		thresHold = 0;
+		for (int i = 0; i < list_sorted_by_tf_idf.size(); i++)
+		{
+			if (list_sorted_by_tf_idf.at(i).size() != 0)
+			{
+				//Retrieve the document d(i) containing term ti that has the next largest tf-idf then remove it
+				documentIndex = list_sorted_by_tf_idf.at(i).begin()->first;
+				weight = list_sorted_by_tf_idf.at(i).begin()->second;
+				list_sorted_by_tf_idf.at(i).erase(list_sorted_by_tf_idf.at(i).begin());
+			}
+			else
+			{
+				list_sorted_by_tf_idf.erase(list_sorted_by_tf_idf.begin()+i);
+			}
+
+			//Compute its global score by retrieving all s(tj, d(i)) with j?i
+			for (int j = 0; j < list_sorted_by_tf_idf.size(); j++)
+			{
+				if (i != j)
+				{
+					if ((result = findPositionOfDocument(documentIndex, list_sorted_by_tf_idf.at(j))) != -1)
+					{
+						globalWeight += list_sorted_by_tf_idf.at(j).at(result).second;
+						list_sorted_by_tf_idf.at(j).erase(list_sorted_by_tf_idf.at(j).begin() + result);
+					}
+				}
+			}
+
+			globalWeight += weight;
+			thresHold += weight;
+
+			//If R contains less than k documents, add d(i) to the result
+			if (topKDocuments.size() < topK && documentIndex != -1)
+			{
+				topKDocuments.push_back(pair<int, double>(documentIndex, globalWeight));
+			}
+			//Otherwise, if gd(i) is larger than the minimum of the scores of documents in Result, 
+			//replace the document with minimum score in R with d(i).
+			else
+			{
+				if (topKDocuments.at(topKDocuments.size()-1).second < globalWeight && documentIndex != -1)
+				{
+					topKDocuments.erase(topKDocuments.end());
+					topKDocuments.push_back(pair<int, double>(documentIndex, globalWeight));
+				}
+			}
+
+			globalWeight = 0;
+			documentIndex = -1;
+			//We always sort the result by tf_idf 
+			sort(topKDocuments.begin(), topKDocuments.end(), sort_by_tf_idf);
+		}
+	} while (topKDocuments.size() < topK  && topKDocuments.at(topKDocuments.size()-1).second< thresHold && list_sorted_by_tf_idf.size() !=0);
+
+	for (int i = 0; i < topKDocuments.size(); i++)
+	{
+		cout << "documentindex:" << topKDocuments.at(i).first << endl;
+		cout << "rank:" << topKDocuments.at(i).second << endl;
+		cout << endl;
+		topDocuments.push_back(topKDocuments.at(i).first);
+	}
+	return topDocuments;
+}
+
+// This function calculates tf_idf of each token of the query 
+vector<vector<pair<int, double>>> Index::calculateTF_IDF(string query)
+{
+	vector<vector<pair<int, double>>> tf_idfLists;
+	vector<pair<int, double>> vector;
+	double tf, idf;
+	fstream inputStream(postingFilePath);
+	istringstream iss(query);
+	string token;
+	unsigned long long termsNumber = 0;
+	unsigned int documentsNumber = 0;
+
+	//Retrieve Number of terms in the dictionary and Number of Documents in the corpus
+	inputStream.read((char*)&termsNumber, sizeof(unsigned long long));
+	inputStream.read((char*)&documentsNumber, sizeof(unsigned int));
+
+	//Retrieve Documents MetaData
+	DocumentMetaData *documentTable = (DocumentMetaData*)malloc(sizeof(DocumentMetaData)*documentsNumber);
+	inputStream.read((char *)documentTable, sizeof(DocumentMetaData)*documentsNumber);
+
+	//Calculate tf-idf in the loop
+	while (iss >> token)
+	{
+		Term * term = dictionary->getTerm(token);
+		if (term != nullptr)
+		{
+			DocumentTerm * documentTermTable = getTermPostingList(token);
+			for (int i = 0; i < term->documentNumber; i++)
+			{
+				tf = ((double)documentTermTable[i].ftd / (double)documentTable[documentTermTable[i].documentIndex].wordsNumber);
+				idf = (double)documentsNumber / (double)term->documentNumber;
+
+				vector.push_back(pair<int, double>(documentTermTable[i].documentIndex, tf*log10(idf)));
+			}
+
+			sort(vector.begin(), vector.end(), sort_by_tf_idf);
+			tf_idfLists.push_back(vector);
+			vector.clear();
+		}
+	}
+	return tf_idfLists;
+}
+
+int Index::findPositionOfDocument(int documentIndex, vector<pair<int, double>> vector)
+{
+	for (int i = 0; i < vector.size(); i++)
+	{
+		if (vector.at(i).first == documentIndex)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+bool sort_by_tf_idf(const pair<int, double>& left, const pair<int, double>& right)
+{
+	return left.second > right.second;
+}
