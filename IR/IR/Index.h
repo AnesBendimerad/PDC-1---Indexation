01fbe2ffd552fe7cae9312be5c4c7b6404aed1cb
Maha Benabbou <maha.benabbou93@gmail.com>
2015-10-18 22:48:09 +0200

    [Maha]: Add implementation of fagin's algorithm

--- IR/IR/Index.h
+++ IR/IR/Index.h
@@ -3,18 +3,23 @@
 #include "IDictionary.h"
 #include "DocumentTable.h"
 #include "DocumentTerm.h"
+#include <vector>
 class Index : public IIndex {
 private:
 	IDictionary *dictionary;
 	DocumentTable *documentTable;
 	bool finalized;
 	string postingFilePath;
+	DocumentTerm* getTermPostingList(string token);
+	vector<vector<pair<int, double>>> calculateTF_IDF(string query);
+	int findPositionOfDocument(int documentIndex,vector<pair<int, double>> vector);
 public:
 	Index(IDictionary *dictionary,string outputFilePath);
 	int addDocument(DocumentMetaData documentMetaData);
 	void addTerm(string token);
 	void finalize();
-	list<int> search(string querry);
-	DocumentTerm* getTermPostingList(string token);
+	list<int> search(int topK,string query);
 	~Index();
 };
+
+bool sort_by_tf_idf(const pair<int, double>& left, const pair<int, double>& right);
