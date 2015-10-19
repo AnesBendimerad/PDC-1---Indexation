98cc295231cd2ed5c0c4027733087c1fec5544ae
Maha Benabbou <maha.benabbou93@gmail.com>
2015-10-19 23:32:00 +0200

    Test new search and print documentmetadata & globalweight

--- IR/IR/IR.cpp
+++ IR/IR/IR.cpp
@@ -16,11 +16,22 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
-	InMemoryIndexBuilder * indexer = new InMemoryIndexBuilder("C:\\myFiles\\tests");
+	InMemoryIndexBuilder * indexer = new InMemoryIndexBuilder("D:\\repo");
 	IIndex  * index = indexer->createIndex();
 	//IndexLoader * indexLoader = new IndexLoader(DEFAULT_OUTPUT_FILE);
 	//IIndex * index = indexLoader->load();
+	
+	vector<pair<DocumentMetaData, double>> topK = index->search(5, "maha hassan");
+
+	for (int i = 0; i < topK.size(); i++)
+	{
+		cout << "id:" << topK.at(i).first.id << endl;
+		cout << "wordsNumber:" << topK.at(i).first.wordsNumber << endl;
+		cout << "offset:" << topK.at(i).first.offset << endl;
+		cout << "adress:" << topK.at(i).first.address << endl;
+		cout << "rank:" << topK.at(i).second << endl;
+		cout << endl;
+	}
 	
 	return 0;
 }
